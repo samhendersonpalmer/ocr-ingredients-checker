558ba30bdcffd7d1acbba2da5dca7e2f5aa68dd1
samhendersonpalmer <samhendersonpalmer@gmail.com>
2026-03-20 08:57:42 +0000

    Added function for scanning picture tab

--- app.py
+++ app.py
@@ -43,6 +43,56 @@ def render_allergen_tab():
     return selected_allergens
 
 
+def render_scan_tab(reader, selected_allergens):
+    st.subheader("2. Scan the product ingredients")
+
+    uploaded_file = st.file_uploader(
+        label="Take picture of ingredients list",
+        label_visibility="hidden",
+        type=["jpg", "jpeg", "png"],
+    )
+
+    if uploaded_file is None:
+        return
+
+    input_image = Image.open(uploaded_file)
+
+    with st.spinner("Scanning image"):
+        result = run_ocr(reader, input_image)
+        img = np.array(input_image)
+
+        word_records = ocr_to_word_records(result, normalize_text)
+        ingredients = word_records_to_ingredient_records(word_records, DELIMITERS)
+        normalized_allergens = normalize_allergen_list(
+            selected_allergens, normalize_text
+        )
+        ingredients_match = flag_matching_ingredients(ingredients, normalized_allergens)
+        output_img = annotate_matched_ingredients(img, ingredients_match)
+
+        col1, col2 = st.columns([2, 2], vertical_alignment="top")
+
+        with col1:
+            st.image(output_img)
+
+        with col2:
+            positive_allergens = []
+
+            for ingredient in ingredients_match:
+                if ingredient["is_match"]:
+                    # Incase matched_allergen key doesn't exist return empty list
+                    for allergen in ingredient.get("matched_allergens", []):
+                        if allergen not in positive_allergens:
+                            positive_allergens.append(allergen)
+
+            st.markdown("### Potential allergens detected:")
+
+            if positive_allergens:
+                for allergen in positive_allergens:
+                    st.markdown(f"### - :color[{allergen}]{{foreground='#bd500c'}}")
+            else:
+                st.markdown("### :color[None]{foreground='#215F9A'}")
+
+
 tab1, tab2 = st.tabs(["1. Select your allergens", "2. Scan ingredients"])
 
 with tab1:
